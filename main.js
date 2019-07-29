af3616b38dcc60a529d81023d0c4b3d7be74f2f3
bojan30 <bojancuk33@gmail.com>
2019-07-29 16:28:47 +0200

    final animation

--- main.js
+++ main.js
@@ -1,51 +1,48 @@
 let svg = Snap("#svg");
 let data1 = svg.select("#data1");
-let path1 = svg.select("#path1");
-let data2 = svg.select("#data2");
-let path2 = svg.select("#path2");
-let data3 = svg.select("#data3");
-let path3 = svg.select("#path3");
-let data4 = svg.select("#data4");
-let path4 = svg.select("#path4");
-let data5 = svg.select("#data5");
-let path5 = svg.select("#path5");
-let data6 = svg.select("#data6");
-let path6 = svg.select("#path6");
+let tiles = svg.select("#tiles");
+let logos = svg.select("#logos");
+let mainTile = svg.select("#main-tile");
+let mainLogo = svg.select("#main-logo");
+let dataPoints = svg.selectAll(".data");
+let paths = svg.selectAll(".line");
+let durations = [1400, 1800, 1200, 1400, 900, 1300];
+
 //function to animate along the path
 
 animateAlongPath = function (path, element, start, dur, callback) {
-  // Get the path length, so we know how many frames we will animate over
   var len = Snap.path.getTotalLength(path);
   Snap.animate(start, len, function (value) {
-    // movePoint gets the path attributes at a certain frame
     var movePoint = Snap.path.getPointAtLength(path, value);
-
-    // applies the attributes to our element
     element.attr({ cx: movePoint.x, cy: movePoint.y });
   }, dur, mina.linear, function () {
     animateAlongPath(path, element, start, dur, callback);
   });
 };
 
-animateAlongPath(path1, data1, 0, 2600, () => {
-  console.log("over");
-})
-
-animateAlongPath(path2, data2, 0, 2400, () => {
-  console.log("over");
-})
-
-animateAlongPath(path3, data3, 0, 2600, () => {
-  console.log("over");
-})
-
-animateAlongPath(path4, data4, 0, 2200, () => {
-  console.log("over");
-})
-
-animateAlongPath(path5, data5, 0, 1800, () => {
-  console.log("over");
-})
-animateAlongPath(path6, data6, 0, 1900, () => {
-  console.log("over");
-})
+window.onload = function(){
+  //tiles opacity
+  tiles.animate({ opacity: 1 }, 500, mina.easeinout);
+  //logos opacity
+  setTimeout(()=>{
+    logos.animate({ opacity: 1 }, 500, mina.easeinout)
+  },500)
+  //main tile opacity
+  setTimeout(() => {
+    mainTile.animate({ opacity: 1 }, 500, mina.easeinout)
+  }, 1000);
+  //main logo opacity
+  setTimeout(() => {
+    mainLogo.animate({ opacity: 1 }, 500, mina.easeinout)
+  },1500);
+  //paths and points opacity
+  setTimeout(()=>{
+    paths.animate({ opacity: 1 }, 500, mina.easeinout);
+    dataPoints.animate({opacity: 1}, 500, mina.easeinout, ()=>{
+      //main animation(data flow)
+      dataPoints.forEach((data,i)=>{
+        animateAlongPath(paths[i], data, 0, durations[i] , animateAlongPath);
+      })
+    });
+  }, 2000)
+}
